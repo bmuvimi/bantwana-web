74eb92e4c6ab692529c7dc15cb6cd5c47ebe936b
charrykingz <53337738+charrykingz@users.noreply.github.com>
2023-03-27 15:30:55 +0200

    Add files via upload

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,15 +2,47 @@
 
 @section('content')
     <section class="mt-20 ">
-        <div class="h-screen">
-
+        <div class="h-3/4">
+            <section
+            class="p-6 text-center shadow-lg md:p-12 md:text-left"
+            style="background-image: url('assets/img/our-projects.jpeg')">
+            <div class="flex justify-center">
+              <div class="max-w-3xl">
+                <div
+                  class="m-4 block rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-800 dark:shadow-black/20">
+                  <div class="md:flex md:flex-row">
+                    <div
+                      class="mx-auto mb-6 flex w-36 items-center justify-center md:mx-0 md:w-96 lg:mb-0">
+                      <img
+                        src="{{ URL::to('/assets/img/BZ-HALF-LOGO.PNG') }}"
+                        class="rounded-full shadow-md dark:shadow-black/30"
+                        alt="woman avatar" />
+                    </div>
+                    <div class="md:ml-6">
+                        <h3 class="font-bold text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-cyan-800 from-orange-400 mb-4">OUR PROJECTS</h3>
+                     <p
+                        class="mb-0 font-semibold text-neutral-500 dark:text-neutral-400">
+                        Insika Ya Kusasa
+                      </p>
+                      <p
+                        class="mb-6 font-light text-neutral-500 dark:text-neutral-300">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
+                        quam sapiente molestiae numquam quas, voluptates omnis nulla
+                        ea odio quia similique corrupti magnam.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
 
 
     </section>
     {{-- Project Tiles --}}
     <section>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 mt-20 px-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 mt-10 px-10">
             <!-- Project card -->
             <div class="flex flex-col bg-white drop-shadow  rounded-sm">
                 <img src="{{ URL::to('/assets/img/project2.jpg') }}" class="hover:drop-shadow-lg hover:opacity-70"
@@ -83,6 +115,105 @@
                         More ></a>
                 </div>
             </div>
+        </div>
+    </section>
+    <!-- === TESTIMONIALS === --->
+    <section class="mt-20 ">
+        <div class="h-screen">
+            <section class="text-neutral-700 dark:text-neutral-300">
+                <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+                  <h3 class="mb-6 text-3xl font-bold">Testimonials</h3>
+                  <p class="mb-6 pb-2 md:mb-12 md:pb-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                    error amet numquam iure provident voluptate esse quasi, veritatis
+                    totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                  </p>
+                </div>
+              
+                <div class="grid gap-6 text-center md:grid-cols-3">
+                  <div>
+                    <div
+                      class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700 dark:shadow-black/30">
+                      <div class="h-28 overflow-hidden rounded-t-lg bg-[#9d789b]"></div>
+                      <div
+                        class="mx-auto -mt-12 w-24 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800">
+                        <img
+                          src="{{ URL::to('/assets/img/voices.jpg') }}" />
+                      </div>
+                      <div class="p-6">
+                        <h4 class="mb-4 text-2xl font-semibold">Tsitsi Ndlovu</h4>
+                        <hr />
+                        <p class="mt-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            class="inline-block h-7 w-7 pr-2"
+                            viewBox="0 0 24 24">
+                            <path
+                              d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                          </svg>
+                          Lorem ipsum dolor sit amet eos adipisci, consectetur
+                          adipisicing elit.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div
+                      class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700 dark:shadow-black/30">
+                      <div class="h-28 overflow-hidden rounded-t-lg bg-[#7a81a8]"></div>
+                      <div
+                        class="mx-auto -mt-12 w-24 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800">
+                        <img
+                          src="{{ URL::to('/assets/img/bongani.jpeg') }}" />
+                      </div>
+                      <div class="p-6">
+                        <h4 class="mb-4 text-2xl font-semibold">Bongani Muvimi</h4>
+                        <hr />
+                        <p class="mt-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            class="inline-block h-7 w-7 pr-2"
+                            viewBox="0 0 24 24">
+                            <path
+                              d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                          </svg>
+                          Neque cupiditate assumenda in maiores repudi mollitia
+                          architecto.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div
+                      class="block rounded-lg bg-white shadow-lg dark:bg-neutral-700 dark:shadow-black/30">
+                      <div class="h-28 overflow-hidden rounded-t-lg bg-[#6d5b98]"></div>
+                      <div
+                        class="mx-auto -mt-12 w-24 overflow-hidden rounded-full border-2 border-white bg-white dark:border-neutral-800 dark:bg-neutral-800">
+                        <img
+                          src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp" />
+                      </div>
+                      <div class="p-6">
+                        <h4 class="mb-4 text-2xl font-semibold">John Smith</h4>
+                        <hr />
+                        <p class="mt-4">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            class="inline-block h-7 w-7 pr-2"
+                            viewBox="0 0 24 24">
+                            <path
+                              d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                          </svg>
+                          Delectus impedit saepe officiis ab aliquam repellat rem unde
+                          ducimus.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
         </div>
     </section>
 @endsection
