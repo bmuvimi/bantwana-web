3916debe3ad6c1a46505eb117a701c17626d237c
charrykingz <53337738+charrykingz@users.noreply.github.com>
2023-03-31 13:45:33 +0200

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -176,6 +176,64 @@
                     </div>
                   </div>
                 </div>
+                <div class="grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                  <div class="flex align-start">
+                    <div class="shrink-0">
+                      <div class="p-4 bg-orange-400 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bug" class="w-5 text-white"
+                          role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                          <path fill="currentColor"
+                            d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64s-64-28.7-64-64s28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z">
+                          </path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="grow ml-6">
+                      <p class="font-bold mb-1">District office - Lupane</p>
+                      <p class="text-cyan-800">info@bantwana.co.zw</p>
+                      <p class="text-cyan-800"> 263 8677 009776</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="mb-12 md:mb-0 grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                  <div class="flex align-start">
+                    <div class="shrink-0">
+                      <div class="p-4 bg-orange-400 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="newspaper"
+                          class="w-5 text-white" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                          <path fill="currentColor"
+                            d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64s-64-28.7-64-64s28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z">
+                          </path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="grow ml-6">
+                      <p class="font-bold mb-1">District Office - Nkayi</p>
+                      <!--p class="text-cyan-800">34 Imbalala Way, Selbourne Park, Bulawayo</p-->
+                      <p class="text-cyan-800">info@bantwana.co.zw</p>
+                      <p class="text-cyan-800"> 263 8677 009776</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                  <div class="flex align-start">
+                    <div class="shrink-0">
+                      <div class="p-4 bg-orange-400 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bug" class="w-5 text-white"
+                          role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                          <path fill="currentColor"
+                            d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64s-64-28.7-64-64s28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z">
+                          </path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="grow ml-6">
+                      <p class="font-bold mb-1">District office - Tsholotsho</p>
+                      <p class="text-cyan-800">info@bantwana.co.zw</p>
+                      <p class="text-cyan-800"> 263 8677 009776</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
